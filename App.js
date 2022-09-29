5da93c09abb65301d71c47cc506eaa09828effd6
bhanu-priyaa <devansh80771@gmail.com>
2022-09-29 17:35:51 +0530

    added files to the repository

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View , Pressable } from 'react-native';
+import Icon from "react-native-ico-material-design";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+var iconHeight = 26;
+var iconWidth = 26;
+
+
+export default class App extends React.Component {
+  state={
+    screenText: 'Press a button'
+  }
+  changeText = (text) => {
+    console.log(text + ' is pressed')
+    this.setState({screenText: text})
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text style={{ fontSize: 30, color: "white" }}>
+            {this.state.screenText}
+          </Text>
+          <StatusBar style="light" />
+        </View>
+        <View style={styles.NavContainer}>
+          <View style={styles.NavBar}>
+            <Pressable
+              onPress={() => this.changeText("Login")}
+              style={styles.IconBehave}
+              android_ripple={{ borderless: true, radius: 50 }}
+            >
+              <Icon
+                name="round-account-button-with-user-inside"
+                height={iconHeight}
+                width={iconWidth}
+                color="#448aff"
+              />
+            </Pressable>
+            <Pressable
+              onPress={() => this.changeText("Sign up")}
+              style={styles.IconBehave}
+              android_ripple={{ borderless: true, radius: 50 }}
+            >
+              <Icon
+                name="add-user-button"
+                height={iconHeight}
+                width={iconWidth}
+                color="#448aff"
+              />
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  NavContainer: {
+    position: 'absolute',
+    alignItems: 'center',
+    bottom: 20,
+  },
+  NavBar: {
+    flexDirection:'row',
+    backgroundColor: '#eee',
+    width: '90%',
+    justifyContent: 'space-evenly',
+    borderRadius: 40,
+  },
+  IconBehave: {
+    padding: 14,
+  }
 });
